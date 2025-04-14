cba82d23e8957be19d2fd62a0a22fcbcb28a2e79
Jay <Jaytjebell@icloud.com>
2025-04-14 13:58:25 +0200

    Update UserResource.php

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -29,6 +29,11 @@ class UserResource extends Resource
     protected static ?string $pluralModelLabel = 'Users';
     protected static ?string $searchLabel = 'Search Users';
 
+    public static function getNavigationBadge(): ?string
+    {
+        return static::getModel()::count();
+    }
+
     public static function form(Form $form): Form
     {
         return $form
