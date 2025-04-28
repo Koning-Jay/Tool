908f68f094c99d50b85b4b2968af278e0962cae8
Jay <Jaytjebell@icloud.com>
2025-04-28 15:51:57 +0200

    Custom chec

--- app/Models/Customchecks.php
+++ app/Models/Customchecks.php
@@ -4,7 +4,6 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
-// Examen github pust Customchecks Ui gemaakt nog geen informatie van de webshopzelf
 
 class Customchecks extends Model
 {
