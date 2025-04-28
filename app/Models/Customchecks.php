70594119925c1f22d45d2685a466ba9096486609
Jay <Jaytjebell@icloud.com>
2025-04-28 10:54:55 +0200

    Ui aangemaakt Checks_gedeelde + Assignoptie gemaakt
    Het begin van het examen voor duidelijkheid

--- app/Models/Customchecks.php
+++ app/Models/Customchecks.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+// Examen github pust Customchecks Ui gemaakt nog geen informatie van de webshopzelf
 
 class Customchecks extends Model
 {
